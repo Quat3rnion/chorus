7528f10636bfea46c9242bc19affd0f8ccad165e
Quat3rnion <quat3rnion@proton.me>
2024-07-15 01:29:32 -0400

    Add payout and membership details to teams

--- src/types/entities/team.rs
+++ src/types/entities/team.rs
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use crate::types::entities::User;
 use crate::types::Snowflake;
@@ -17,12 +18,51 @@ pub struct Team {
     pub members: Vec<TeamMember>,
     pub name: String,
     pub owner_user_id: Snowflake,
+    pub payout_account_status: Option<TeamPayoutAccountStatus>,
+    pub stripe_connect_account_id: Option<String>,
+}
+
+#[derive(Serialize_repr, Deserialize_repr, Debug, Default, Clone, Eq, PartialEq, Hash, Copy)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+#[repr(u8)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum TeamPayoutAccountStatus {
+    #[default]
+    Unsubmitted = 1,
+    Pending = 2,
+    ActionRequired = 3,
+    Active = 4,
+    Blocked = 5,
+    Suspended = 6,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::FromRow))]
 pub struct TeamMember {
-    pub membership_state: u8,
-    pub permissions: Vec<String>,
+    pub membership_state: MembershipState,
     pub team_id: Snowflake,
+    #[cfg_attr(feature = "sqlx", sqlx(skip))]
     pub user: Shared<User>,
+    pub role: TeamMemberRole,
+}
+
+#[derive(Serialize_repr, Deserialize_repr, Debug, Default, Clone, Eq, PartialEq, Hash, Copy)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+#[repr(u8)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum MembershipState {
+    #[default]
+    Invited = 1,
+    Accepted = 2,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Eq, PartialEq, Hash, Copy)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+#[serde(rename_all = "snake_case")]
+pub enum TeamMemberRole {
+    Owner,
+    Admin,
+    Developer,
+    #[default]
+    ReadOnly
 }
